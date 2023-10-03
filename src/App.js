d3491c580a947fbb87115e17bb24896293a02bb1
devGojo <ra38834258851@acaonsfatima.org.br>
2023-10-03 07:28:29 -0300

    standing by

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { useState } from 'react';
+import smirk from '../src/assets/images/bad-yellow.png';
+import slogann from '../src/assets/images/yellow bad 1.png';
+import profile from '../src/assets/images/profile.png';
+import black from '../src/assets/images/profileblack.png';
+import edit from '../src/assets/images/edit.png';
+import pending from '../src/assets/images/pending.png';
+import axios from 'axios';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     <main>
+      <header>
+        <img src={smirk} className='smirk' />
+        <img src={slogann} className='slogan'/>
+        <img src={black} className='Black'/>
+        <button className='acess'>Acessar a loja</button>
       </header>
-    </div>
+      <section>
+          <div className='info'>
+            <p>Informações da conta</p>
+          </div>
+
+          <div className='left'> 
+          <img src={profile} className='profile'/> <h1>Perfil</h1>
+          
+            <h1>Pedidos</h1><img src={pending} className='pending'/>
+          </div>
+          
+                <div className='domo'>
+                  <span>Nome: </span> <br />
+                  <span>Tel: </span> <br />
+                 <span>CEP: </span> <br />
+                <span>Email: </span> <br /> <br />
+                <span>Senha: </span><br />
+                </div>
+
+          <div>
+            <button className='GetOff'>Sair</button>
+          </div>
+
+      </section>
+    </main>
   );
 }
 
